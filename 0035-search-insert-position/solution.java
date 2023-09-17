5ceafe8b557a66412dbafa457e564e0bf809f65f
PRAKADESH <84655612+prakadesh@users.noreply.github.com>
2023-09-17 15:43:04 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 43.3 MB (49.79%)

---
+++ 0035-search-insert-position/solution.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int l = 0;
+        int e= nums.length;
+        int r = e-1;
+
+        while (l <= r){
+            int m = l+ (r-l) /2 ;
+
+            if (nums[m]== target){
+                return m;
+            }else if (nums[m]>target){
+                r=m-1;
+            }else{
+                l=m+1;
+            }
+        }
+
+
+        return l;
+    }
+}
